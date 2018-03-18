0f753e92c18dc1dc10474dddfdd528544e7784e3
anosv <crazzyzyzy@rambler.ru>
2018-03-18 20:51:41 +0300

    refactoring allure utils

--- src/main/java/ru/sbtqa/smartly/utils/AllureUtils.java
+++ src/main/java/ru/sbtqa/smartly/utils/AllureUtils.java
@@ -12,26 +12,23 @@ public final class AllureUtils {
 
     public static Logger log = Logger.getRootLogger();
     private static final String PATH_ENV_PROP = "target/allure-results/";
-    private static final String NAME_ENV_FILE = "environment.properties";
+    private static final String NAME_ENV_FILE = PATH_ENV_PROP + "environment.properties";
 
     private AllureUtils() throws IllegalAccessException {
         throw new IllegalAccessException("AllureUtils is utility class!");
     }
 
     public static void createEnvironmentProperties() {
-        try {
+        try (FileOutputStream fos = new FileOutputStream(NAME_ENV_FILE)){
             if(!Files.exists(Paths.get(PATH_ENV_PROP))){
                 Files.createDirectories(Paths.get(PATH_ENV_PROP));
             }
 
             Properties props = new Properties();
-            FileOutputStream fos = new FileOutputStream(PATH_ENV_PROP + NAME_ENV_FILE);
 
             Optional.ofNullable("Chrome").ifPresent(s -> props.setProperty("Browser", s));
 
             props.store(fos, "-- NO COMMENTS--");
-
-            fos.close();
         } catch (IOException e) {
             log.error("AllureUtils createEnvironmentProperties IOException", e);
         }
